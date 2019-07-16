9de5e956cc1eb3ff9b4d5078441eac5c0e74f7d4
LeQuangDuy <thedblaster@gmail.com>
2019-07-16 20:15:18 +0700

    Add device to token, store token on Redis

--- src/services/users.login.js
+++ src/services/users.login.js
@@ -1,29 +1,32 @@
 const findUserByCredentials = require('../database/users.findByCredentials');
-const jwt = require('jsonwebtoken');
-const secret = require('../config/jwtSecret');
+const generateToken = require('../services/token.generate');
+
+const validCredentials = (req) => {
+  if (!req.body.email || !req.body.password)
+    return {
+      error: {
+        isError: true,
+        errorMessage: {
+          credentials: 'Email or password is missing'
+        }
+      }
+    };
+}
+
+
 
 module.exports = async (req, res, next) => {
-  if (!req.body.email || !req.body.password) return res.status(400).send({
-    error: {
-      isError: true,
-      errorMessage: {
-        credentials: 'Email or password is missing'
-      }
-    }
-  });
-
+  const invalid = validCredentials(req);
+  if (invalid) {
+    return res.status(400).send(valid);
+  }
   try {
     const user = await findUserByCredentials(req.body.email, req.body.password);
     if (user.error) {
       return res.status(400).send({ error: user.error });
     }
-    res.send({
-      'access-token': jwt.sign({ id: user._id }, secret, { expiresIn: '20s' }),
-      error: {
-        isError: false,
-        errorMessage: {}
-      }
-    })
+    const platform = 'web';
+    generateToken(user._id, platform, res);
   } catch (e) {
     console.log(e.message);
     res.status(500).send({
