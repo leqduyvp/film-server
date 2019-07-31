1c7a9813177b03b8738755557522236bf8904ab6
Le Quang Duy <thedblaster@gmail.com>
2019-07-31 12:00:23 +0700

    Add admin token test for banner and category

--- tests/banner.test.js
+++ tests/banner.test.js
@@ -1,9 +1,14 @@
 const request = require('supertest');
+const jwt = require('jsonwebtoken');
 
 const app = require('../server');
+const secret = require('../config/jwtSecret');
 const Banner = require('../database/Banner.model');
 
 const { setupDatabase, bannerInit } = require('./banner.dataInit');
+const { validAdminUser } = require('./users.dataInit');
+
+const adminToken = jwt.sign({ id: validAdminUser._id, platform: 'web', accType: 0 }, secret);
 
 beforeAll(setupDatabase);
 
@@ -11,11 +16,12 @@ test('Should get all banners', async () => {
   const response = await request(app).get('/api/banners')
     .send()
     .expect(200);
+  expect(response.body.error.isError).toBeFalsy();
 });
 
 test('Should add new banner(action: link)', async () => {
   const response = await request(app).post('/api/banners')
-    .set('access-token', 'Sua cho nay')
+    .set('access-token', adminToken)
     .send({
       "image": "https://image.com/banner2",
       "action": "link",
@@ -32,7 +38,7 @@ test('Should add new banner(action: link)', async () => {
 
 test('Should add new banner(action: tag)', async () => {
   const response = await request(app).post('/api/banners')
-    .set('access-token', 'Sua cho nay')
+    .set('access-token', adminToken)
     .send({
       "image": "https://image.com/banner3",
       "action": "tag",
@@ -46,9 +52,24 @@ test('Should add new banner(action: tag)', async () => {
   expect(banner).toBeTruthy();
 });
 
+test('Should not add new banner for false token', async () => {
+  const response = await request(app).post('/api/banners')
+    .set('access-token', adminToken + '3')
+    .send({
+      "image": "https://image.com/banner2",
+      "action": "link",
+      "payload": {
+        "id": "3hjhu3h4u3h4u33uh",
+        "title": "Biệt đội siêu anh hùng"
+      }
+    })
+    .expect(500);
+  expect(response.body.error.isError).toBeTruthy();
+});
+
 test('Should not add new banner with invalid information', async () => {
   const response = await request(app).post('/api/banners')
-    .set('access-token', 'Sua cho nay')
+    .set('access-token', adminToken)
     .send({})
     .expect(400);
   expect(response.body.error.isError).toBeTruthy();
@@ -64,7 +85,7 @@ test('Should not add new banner with invalid information', async () => {
 
 test('Should change invalid banner', async () => {
   const response = await request(app).patch('/api/banners')
-    .set('access-token', 'Sua cho nay')
+    .set('access-token', adminToken)
     .query({ id: bannerInit._id.toString() })
     .send({
       image: 'image.com/newImage',
@@ -79,9 +100,23 @@ test('Should change invalid banner', async () => {
   expect(banner.action).toBe('tag');
 });
 
+test('Should not change banner with invalid token', async () => {
+  const response = await request(app).patch('/api/banners')
+    .query({ id: bannerInit._id.toString() })
+    .set('access-token', adminToken + '3')
+    .send({
+      image: 'image.com/newImage',
+      action: 'tag',
+      payload: {
+        tag: 'Hell'
+      }
+    });
+  expect(response.body.error.isError).toBeTruthy();
+});
+
 test('Should not change banner with invalid id', async () => {
   const response = await request(app).patch('/api/banners')
-    .set('access-token', 'Sua cho nay')
+    .set('access-token', adminToken)
     .send({
       image: 'image.com/newImage',
       action: 'tag',
@@ -95,7 +130,7 @@ test('Should not change banner with invalid id', async () => {
 
 test('Should not change banner with invalid update', async () => {
   const response = await request(app).patch('/api/banners')
-    .set('access-token', 'Sua cho nay')
+    .set('access-token', adminToken)
     .query({ id: bannerInit._id.toString() })
     .send({
     })
@@ -105,7 +140,7 @@ test('Should not change banner with invalid update', async () => {
 
 test('Should delete banner with correct id', async () => {
   const response = await request(app).delete('/api/banners')
-    .set('access-token', 'Sua cho nay')
+    .set('access-token', adminToken)
     .query({ id: bannerInit._id.toString() })
     .send()
     .expect(200);
@@ -114,9 +149,17 @@ test('Should delete banner with correct id', async () => {
 
 test('Should not delete banner with invalid id', async () => {
   const response = await request(app).delete('/api/banners')
-    .set('access-token', 'Sua cho nay')
+    .set('access-token', adminToken)
     .query({ id: '123123123' })
     .send()
     .expect(500);
+  expect(response.body.error.isError).toBeTruthy();
+})
+
+test('Should not delete banner with invalid token', async () => {
+  const response = await request(app).delete('/api/banners')
+    .set('access-token', adminToken + '3')
+    .query({ id: bannerInit._id })
+    .send();
   expect(response.body.error.isError).toBeTruthy();
 })
